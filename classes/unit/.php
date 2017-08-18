2bd644fc79c59a20e55d57e3e2d0c0182e2e14f1
VitaliiBondarenko <bndarenkvit@gmail.com>
2017-08-18 20:16:53 +0300

    Add project

---
+++ classes/unit/.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: User
+ * Date: 29.07.11
+ * Time: 13:19
+ * To change this template use File | Settings | File Templates.
+ */
+ 
---
+++ js/ckeditor/plugins/more/dialogs/more.js
@@ -0,0 +1,30 @@
+﻿CKEDITOR.dialog.add('more', function(editor) {
+	return {
+		title : 'Создать раскрывающийся блок',
+		minWidth : 400,
+		minHeight : 200,
+		onOk: function() {
+			var cuttext = this.getContentElement('more', 'cuttext').getInputElement().getValue();
+			var cutsize = this.getContentElement('more', 'cutsize').getInputElement().getValue();
+			var selectedText = editor.getSelectedHtml();
+
+			this._.editor.insertHtml('<div class="ckmore" title="'+ cuttext +'" rel="' + cutsize + '">' + selectedText + '</more>');
+		},
+		contents : [{
+			id : 'more',
+			label : 'First Tab',
+			title : 'First Tab',
+			elements : [{
+				id : 'cuttext',
+				type : 'text',
+				label : 'Текст ссылки'
+			}, {
+				id : 'cutsize',
+				type : 'text',
+				label : 'Количество символов'
+			}
+			]
+		}]
+	};
+});
+
